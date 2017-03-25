2836f59d7e5f36ab2becdc2be24573793c3e8998
BipinPaudel <paudelbipin.bp@gmail.com>
2017-03-25 14:26:30 +0545

    changes

--- scrape_wayfair.py
+++ scrape_wayfair.py
@@ -1,53 +1,71 @@
+import time
 def checkShowAllImage(soup):
     allImg=soup.find_all('img',{"src":True})
-    
     imagesList=[]
     for a in allImg:
         if a['src'].endswith("/2916/29161613/default_name.jpg") or a['src'].endswith("/2973/29734734/default_name.jpg"):
-            print(a['src'])
             imagesList.append(a['src'])
-    print(len(imagesList))
     return imagesList
 
-def grabEverythingFromProductList(productListUrl):
+def grabEverythingFromProductList(productUrl):
+    productListUrl=productUrl
     wholeLinkList=[]
     while True:
         driver.get(productListUrl)
         html = driver.page_source
         soup = BeautifulSoup(html,"lxml")
         productThumbNail=soup.find_all("div",{"id":"sbprodgrid"})
-        aList=productThumbNail[0].find_all("a",{"class":"SbProductBlock js-product-bottom-link js-prod-content"})
+        print(productThumbNail)
+        try:
+            aList=productThumbNail[0].find_all("a",{"class":"SbProductBlock js-product-bottom-link js-prod-content "})
+        except:
+            time.sleep(5)
+            continue
         for a in aList:
             print(a['href'])
             wholeLinkList.append(a['href'])
         if soup.find_all("span",{"class":"Pagination-item is-inactive Pagination-icon--next js-next-page"}):
             break
-        productListUrl=soup.find("a",{"class":"js-next-page Pagination-item Pagination-icon--next Pagination-link"})['href']
-        
+        try:
+
+            productListUrl=soup.find("a",{"class":"js-next-page Pagination-item Pagination-icon--next Pagination-link"})['href']
+
+        except:
+            continue
+    print("There are total "+ str(len(wholeLinkList)) +"items in this category");
+    
     for singleItemUrl in wholeLinkList:
         driver.get(singleItemUrl)
         html=driver.page_source
         soup=BeautifulSoup(html,"lxml")
         productTitle=soup.find("span",{"class":"ProductDetailInfoBlock-header-title js-product-title-name"})
-        productPrice=soup.find("span",{"data-id":"dynamic-sku-price"})
+        productPriceHead=soup.find("div",{"class":"ProductDetailInfoBlock-pricing-amount js-price-format"})
+        productPrice = soup.find("span",{"class":""})
         productUrl=singleItemUrl
         productImageUrl=soup.find("img",{"class":"ProductDetailImagesBlock-carousel-image"})
         productImageUrl=productImageUrl['src'].replace("%20","")
-        productDescription=soup.find("div",{"class":"ProductDetailInfoBlock-snippets clearfix"})
-        ulList=productDescription.find("ul")
+        
+        productDescription=soup.find("div",{"class":"js-content-contain"})
+        descriptionSplit=productDescription.find("p")
         description=''
-        liInformation=ulList.find_all('li')
-        for a in liInformation:
-            description+=a.text+" "
-    
-        print(productTitle.text+productPrice.text)
-        outputWriter.writerow([productTitle,productPrice,productUrl,productImageUrl,description])
+        features=productDescription.find_all("li")
+
+        for p in descriptionSplit:
+      
+            description+= str(p)+' '
+        description+="Features\n"
+        for li in features:
+            reqstring1=str(li.text)
+            description+=str(reqstring1)+', '
+        
+        outputWriter.writerow([productTitle.text,productPrice.text,productUrl,productImageUrl,description])
+        print("This product written")
 
 
 
-#for home page
+
 import csv
-outputFile = open('output.csv', 'w', newline='')
+outputFile = open('output.csv', 'a', newline='')
 outputWriter = csv.writer(outputFile)
 outputWriter.writerow(['product title','product price','product url','product image','product description'])
 homeUrl="https://www.wayfair.com"
@@ -57,31 +75,34 @@ driver=webdriver.Firefox()
 driver.get(homeUrl)
 html=driver.page_source
 homeSoup=BeautifulSoup(html,"lxml")
-
+print("We are going to the index page header to extract all categories")
 mainHeadingBlock=homeSoup.find_all("div",{"class":"nav_link_block_links nav_link_block_text"})
 mainLinks=[]#contains all link from header bar
 for req in mainHeadingBlock:
     hrefsFromHeader=req.find_all("a",{"class":"js-cms-link cms_add_link "})
     for headerLinkElement in hrefsFromHeader:
         mainLinks.append(headerLinkElement['href'])
-print(len(mainLinks))
+print("Categories are extracted and will be used further")
+print("there are total "+str(len(mainLinks)) + "categories")
 
-
-for categoryUrl in mainLinks:
+i=0
+for categoryUrl in mainLinks[i:]:
     driver.get(categoryUrl)
     html=driver.page_source
     soup=BeautifulSoup(html,"lxml")
     imageList=checkShowAllImage(soup)
-    if len(soup.find_all("div",{"id":"sbprodgrid"}))>0:
+    print(str(i) + " position " + str(categoryUrl) + " has been started----------------")
+    if len(soup.find_all("a",{"class":"sbprodgrid"}))>0:
         grabEverythingFromProductList(categoryUrl)
+    
     elif len(imageList)>0:
-        print('la dherai')
+
         imageTag=soup.find('img',{'src':imageList[0]})
         productListUrl=imageTag.parent['href']
+        print("len imagelist is > 0 " + productListUrl)
         grabEverythingFromProductList(productListUrl)
             
     else:
-        print('ttt')
         allCategoryGrid=homeSoup.find_all("span",{"class":"js-lego-data lego_text_field "})
         for a in allCategoryGrid:
             if a.text.endswith('by Category'):
@@ -102,11 +123,13 @@ for categoryUrl in mainLinks:
                     else:
                         imageList=checkShowAllImage(categorySoup)
                         if len(imageList)>0:
-                            print('ok more')
                             imageTag=soup.find('img',{'src':imageList[0]})
                             productListUrl=imageTag.parent['href']
                             wholeLinkList=[]
                             grabEverythingFromProductList(productListUrl)
+    print(str(i) + "category finished successfully")  
+    i=i+1                      
+
                         
 outputFile.close()
         
